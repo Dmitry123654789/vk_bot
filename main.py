d078a3d31ced8c0e6e2e558e87e12c11f3527a29
Dmitry <pokemon2708@yandex.ru>
2025-05-03 22:46:02 +0300

    Бот Большой Брат

--- main.py
+++ main.py
@@ -1,51 +1,28 @@
 import vk_api
-
-from config import PASSWORD, LOGIN, ALBUM_ID, GROUP_ID
-
-
-def captcha_handler(captcha):
-    """ При возникновении капчи вызывается эта функция и ей передается объект
-        капчи. Через метод get_url можно получить ссылку на изображение.
-        Через метод try_again можно попытаться отправить запрос с кодом капчи
-    """
-
-    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-
-    # Пробуем снова отправить запрос с капчей
-    return captcha.try_again(key)
-
-
-def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция. """
-
-    # Код двухфакторной аутентификации,
-    # который присылается по смс или уведомлением в мобильное приложение
-    # или код из приложения - генератора кодов
-    key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
-    remember_device = True
-
-    return key, remember_device
-
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
+from config import TOKEN, GROUP_ID
 
 def main():
-    login, password = LOGIN, PASSWORD
     vk_session = vk_api.VkApi(
-        login, password,
-        # функция для обработки двухфакторной аутентификации
-        auth_handler=auth_handler,
-        captcha_handler=captcha_handler
-    )
+        token=TOKEN)
 
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
+    longpoll = VkBotLongPoll(vk_session, GROUP_ID)
 
-    upload = vk_api.VkUpload(vk_session)
-    for path_img in ['cat.png', 'forest.png']:
-        upload.photo(f'static/img/{path_img}', album_id=ALBUM_ID, group_id=GROUP_ID)
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            vk = vk_session.get_api()
+            user = vk.users.get(user_ids=[event.obj.message["from_id"]], fields="city, bdate, country")[0]
+            city = user["city"]['title'] if "city" in user else None
+            name = user["first_name"]
+
+            vk.messages.send(user_id=user['id'],
+                             message=f"Привет, {name}!",
+                             random_id=random.randint(-9*10**18, 9*10**18))
+            if city:
+                vk.messages.send(user_id=user['id'],
+                                 message=f"Как поживает {city}?",
+                                 random_id=random.randint(-9*10**18, 9*10**18))
 
 
 if __name__ == '__main__':
