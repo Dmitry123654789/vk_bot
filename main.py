7d3f5b71104501a82b4386bace724f1dec62414a
Dmitry <pokemon2708@yandex.ru>
2025-05-06 16:28:07 +0300

    Запрос Статистика

--- main.py
+++ main.py
@@ -1,31 +1,81 @@
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import random
+from flask import Flask, render_template
+from vk_api import ApiError
+from werkzeug.exceptions import BadRequest
 
-from wikipedia import PageError
+from config import PASSWORD, LOGIN
 
-from config import TOKEN, GROUP_ID
-import wikipedia
-def main():
-    vk_session_bot = vk_api.VkApi(
-        token=TOKEN)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-    longpoll = VkBotLongPoll(vk_session_bot, GROUP_ID)
 
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            vk_bot = vk_session_bot.get_api()
-            try:
-                res = wikipedia.summary(event.obj.message["text"], sentences=10)
-                vk_bot.messages.send(user_id=event.obj.message["from_id"],
-                                     message=f"Что вы хотите узнать?\n{res}",
-                                     random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18))
-            except PageError:
-                vk_bot.messages.send(user_id=event.obj.message["from_id"],
-                                     message=f"Что вы хотите узнать?",
-                                     random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18))
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция. """
+
+    # Код двухфакторной аутентификации,
+    # который присылается по смс или уведомлением в мобильное приложение
+    # или код из приложения - генератора кодов
+    key = input("Enter authentication code: ")
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
+
+    return key, remember_device
+
+
+@app.route('/vk_stat/<int:group_id>')
+def admin_page(group_id):
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(
+        login, password,
+        # функция для обработки двухфакторной аутентификации
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
+    )
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
+
+    try:
+        res = vk.stats.get(group_id=group_id, intervals_count=10, extended=True)[0]
+    except ApiError:
+        return BadRequest()
+
+    try:
+        cities = list(set([x['name'] for x in res['visitors']['cities']]))
+    except KeyError:
+        cities = []
+
+    activities = {}
+    for x in ['likes', 'comments', 'subscribed']:
+        try:
+            activities[x] = res['activity'][x]
+        except KeyError:
+            activities[x] = 0
+    ages_j = res['reach']['age']
+    ages = {}
+    for n, x in enumerate(['12-18', '18-21', '21-24', '24-27', '27-30', '30-35', '35-45', '45-100']):
+        try:
+            ages[x] = ages_j[n]['count']
+        except KeyError:
+            ages[x] = 0
+    return render_template('index.html', cities=cities, activities=activities, ages=ages)
 
 
 if __name__ == '__main__':
-    wikipedia.set_lang('ru')
-    main()
+    app.run(port=8080, host='127.0.0.1')
