4f2b48ff6cfa0c54ad94f0882a8cb175c08bcba8
Dmitry <pokemon2708@yandex.ru>
2025-05-07 18:06:23 +0300

    Бот-геокодер с клавиатурой

--- main.py
+++ main.py
@@ -1,81 +1,112 @@
+import random
+from json import loads
+from os import remove
+
+import requests
 import vk_api
-from flask import Flask, render_template
-from vk_api import ApiError
-from werkzeug.exceptions import BadRequest
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-from config import PASSWORD, LOGIN
+from config import TOKEN, GROUP_ID
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-def captcha_handler(captcha):
-    """ При возникновении капчи вызывается эта функция и ей передается объект
-        капчи. Через метод get_url можно получить ссылку на изображение.
-        Через метод try_again можно попытаться отправить запрос с кодом капчи
-    """
-
-    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-
-    # Пробуем снова отправить запрос с капчей
-    return captcha.try_again(key)
+LAST_MES = ''
+LAST_COM = ''
 
 
-def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция. """
-
-    # Код двухфакторной аутентификации,
-    # который присылается по смс или уведомлением в мобильное приложение
-    # или код из приложения - генератора кодов
-    key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
-    remember_device = True
-
-    return key, remember_device
+def get_json(adress):
+    api_key = '8013b162-6b42-4997-9691-77b7074026e0'
+    server_address = 'https://geocode-maps.yandex.ru/1.x/?'
+    geocoder_request = f'{server_address}apikey={api_key}&geocode={adress}&format=json'
+    response = requests.get(geocoder_request)
+    return response
 
 
-@app.route('/vk_stat/<int:group_id>')
-def admin_page(group_id):
-    login, password = LOGIN, PASSWORD
+def get_response_map(ll):
+    server_address = "https://static-maps.yandex.ru/v1"
+    apikey = '0eea7a3e-806e-4b45-8976-3c543752e89c'
+    map_params = {
+        'll': ll,
+        'apikey': apikey,
+        'maptype': LAST_COM,
+        'z': 15,
+        'lang': 'ru_RU'
+    }
+    session = requests.Session()
+    retry = Retry(total=10, connect=5, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    response = requests.get(server_address, params=map_params)
+    return response
+
+
+def main():
+    global LAST_MES, LAST_COM
     vk_session = vk_api.VkApi(
-        login, password,
-        # функция для обработки двухфакторной аутентификации
-        auth_handler=auth_handler,
-        captcha_handler=captcha_handler
-    )
+        token=TOKEN)
 
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-    vk = vk_session.get_api()
+    longpoll = VkBotLongPoll(vk_session, GROUP_ID)
 
-    try:
-        res = vk.stats.get(group_id=group_id, intervals_count=10, extended=True)[0]
-    except ApiError:
-        return BadRequest()
+    for event in longpoll.listen():
 
-    try:
-        cities = list(set([x['name'] for x in res['visitors']['cities']]))
-    except KeyError:
-        cities = []
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            try:
+                if LAST_COM == 'type':
+                    LAST_COM = loads(event.object['message']['payload'])['command']
+            except KeyError:
+                LAST_COM = ''
 
-    activities = {}
-    for x in ['likes', 'comments', 'subscribed']:
-        try:
-            activities[x] = res['activity'][x]
-        except KeyError:
-            activities[x] = 0
-    ages_j = res['reach']['age']
-    ages = {}
-    for n, x in enumerate(['12-18', '18-21', '21-24', '24-27', '27-30', '30-35', '35-45', '45-100']):
-        try:
-            ages[x] = ages_j[n]['count']
-        except KeyError:
-            ages[x] = 0
-    return render_template('index.html', cities=cities, activities=activities, ages=ages)
+            vk = vk_session.get_api()
+            if LAST_COM == '':
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message="Что вы хотите увидеть?",
+                                 random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18),
+                                 keyboard='{"one_time": false, "buttons": []}')
+                LAST_COM = 'que'
+
+            elif LAST_COM == 'que':
+                LAST_MES = event.object.message['text']
+
+                try:
+                    res = get_json(LAST_MES).json()['response']['GeoObjectCollection']['featureMember']
+                    if len(res) == 0:
+                        LAST_COM = 'que'
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                         message="Что вы хотите увидеть?",
+                                         random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18),
+                                         keyboard='{"one_time": false, "buttons": []}')
+                    else:
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                         message="Какой тип карты вы хотите увидеть?",
+                                         random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18),
+                                         keyboard=open('buttons.json', encoding='utf-8').read())
+                        LAST_COM = 'type'
+                except Exception as e:
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message="Повторите запрос.\nЧто вы хотите увидеть",
+                                     random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18),
+                                     keyboard='{"one_time": false, "buttons": []}')
+                    LAST_COM = 'que'
+
+
+            elif LAST_COM in ['admin', 'transit', 'driving', 'map']:
+                res = get_json(LAST_MES).json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+                poi = ','.join(res['Point']['pos'].split(' '))
+                place = res['metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['AddressLine']
+
+                open('map.png', 'wb').write(get_response_map(poi).content)
+                upload = vk_api.VkUpload(vk_session)
+                photo = upload.photo_messages(photos='map.png')[0]
+                attachment = f"photo{photo['owner_id']}_{photo['id']}"
+                remove("map.png")
+
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=f'Это {place}. Что вы еще хотите увидеть?',
+                                 random_id=random.randint(-9 * 10 ** 18, 9 * 10 ** 18),
+                                 attachment=attachment,
+                                 keyboard='{"one_time": false, "buttons": []}')
+                LAST_COM = 'que'
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
